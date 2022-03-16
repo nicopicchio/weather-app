f231583a70aab231d6ac7f60ca6bcae9b486f4e7
Nico Picchio <nicolapicchio@gmail.com>
2022-03-16 11:32:09 +0000

    fetching weather for location

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import { useState, useEffect } from 'react';
+import { logo } from './assets/logo.png';
 
 function App() {
 	const [location, setLocation] = useState('');
 	const [woeID, setWoeID] = useState(0);
-	console.log('RENDERING WEATHER APP', woeID);
+	const [weather, setWeather] = useState({});
+	console.log('RENDERING WEATHER APP', weather);
 
-	// new object for json response
 	useEffect(() => {
 		fetch(`http://localhost:3001/api/location/search/?query=${location}`)
 			.then((res) => res.json())
 			.then((jsonResponse) => setWoeID(jsonResponse[0].woeid));
 	}, [location]);
+
+	useEffect(() => {
+		fetch(`http://localhost:3001/api/location/${woeID}`)
+			.then((res) => res.json())
+			.then((jsonResponse) => setWeather(jsonResponse));
+	}, [woeID]);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -20,10 +27,11 @@ function App() {
 	return (
 		<>
 			<div className='container'>
+				{/* <img id='logo' alt='logo' src={logo} /> */}
 				<h1>Weather App</h1>
 				<form className='location-form' onSubmit={handleSubmit}>
 					<label className='input-label'>
-						Please enter location
+						Location
 						<input type='text' className='search-input' name='city' required />
 						<input type='submit' className='submit-button' value='Search' />
 					</label>
